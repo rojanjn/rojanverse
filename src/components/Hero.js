9080b656c4e0cc927814e53e748fb595db447dad
Rojan Jafarnezhad <rojan.jafarnezhad@georgebrown.ca>
2026-01-01 13:08:53 -0500

    Add Hero component with welcome message

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Hero() {
+    return (
+        <section className="hero">
+            <h2>Welcome to My Portfolio!</h2>
+            <p>I'm a developer and photographer. Scroll down to explore more.</p>
+        </section>
+    );
+}
+
+export default Hero;
