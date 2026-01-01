278c44baceefd8dca1a2d146a2009dcdec4e04a4
Rojan Jafarnezhad <rojan.jafarnezhad@georgebrown.ca>
2026-01-01 13:15:04 -0500

    Add PhotoGallery component to display a collection of images

---
+++ src/components/PhotoGallery.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function PhotoGallery() {
+    return (
+        <section id="photos" className="photo-gallery">
+            <h2>Photo Gallery</h2>
+            <p>Here are some of my favorite shots:</p>
+            <div className="gallery">
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+            </div>
+        </section>
+    );
+}
+
+export default PhotoGallery;
