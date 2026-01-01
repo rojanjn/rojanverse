90dc55c30231389eadd5b0cc8a11467ca7a797f1
Rojan Jafarnezhad <rojan.jafarnezhad@georgebrown.ca>
2026-01-01 13:16:39 -0500

    Add Footer component to display copyright information

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <p>&copy; 2026 Rojan Jafarnezhad. All rights reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer;
