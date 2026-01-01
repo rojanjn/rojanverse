f7222894ab795c6c461a8e80e9a3a1038cc950b6
Rojan Jafarnezhad <rojan.jafarnezhad@georgebrown.ca>
2026-01-01 13:13:03 -0500

    Add Projects component to showcase personal projects

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function About() {
-    return(
+    return (
         <section id="about" className="about">
             <h2>About Me</h2>
             <p>Hi, I'm a developer passionate about coding and taking photos. I love exploring vibrant designs and creating games and projects.</p>
---
+++ src/components/Projects.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Projects() {
+    return (
+        <section id="projects" className="projects">
+            <h2>My Projects</h2>
+            <ul>
+                <li>Project 1: Portfolio Website</li>
+                <li>Project 2: Photo Management App</li>
+            </ul>
+        </section>
+    );
+}
+
+export default Projects;
