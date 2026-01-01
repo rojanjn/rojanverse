6c62deb2d9c186973642be6d633befb63939676a
Rojan Jafarnezhad <rojan.jafarnezhad@georgebrown.ca>
2026-01-01 13:07:12 -0500

    Add Navbar component with navigation links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,17 @@
-impo
+import React from "react";
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <h1>My Portfolio</h1>
+            <ul>
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#photos">Photos</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
