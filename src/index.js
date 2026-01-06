cfc6277dab0ea435cd9cc728a69511d7f7c11983
Rojan Jafarnezhad <rojan.jafarnezhad@georgebrown.ca>
2026-01-06 15:35:55 -0500

    restarting the project.

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-doc/client";
-import App from "./App";
-import "./styles.css";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
