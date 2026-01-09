c7f491519af196e546773193fed2f052f0a7cd18
Rojan Jafarnezhad <rojan.jafarnezhad@georgebrown.ca>
2026-01-09 18:46:05 -0500

    feat: initialize rojanverse project with Vite and React setup

---
+++ components/Hero.tsx
@@ -0,0 +1,16 @@
+import { Github, Linkedin, Mail, Twitter } from 'lucide-react';
+
+export function Hero() {
+    return (
+        <section className="">
+            <div className="">
+                <div className="">
+                    <div className="">
+                        <p className="text-muted-foreground">Hello there, I'm</p>
+                        <h1 className="text 5xl">Rojan</h1>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
